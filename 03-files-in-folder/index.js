f07a809b2361d26e1a60e814c7ba4402cacdf729
leonarci <artem.janicki@gmail.com>
2022-05-23 22:48:16 +0300

    refactor: 03-files-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,16 +3,21 @@ const path = require('path');
 
 const folder = path.join(__dirname, 'secret-folder');
 
-readdir(folder, {withFileTypes: true})
-  .then((value) => {
-    value.forEach(file => {
-      if (file.isFile()) {
-        const name = file.name.slice(0, file.name.lastIndexOf('.'));
-        const ext = path.extname(file.name).slice(1);
-        stat(path.join(folder, file.name)).then( value => {
-          const size = (value.size / 1024).toFixed(2) + 'kb';
-          console.log(`${name} - ${ext} - ${size}`);
-        });
-      }
-    });
-  });
+try {
+  readdir(folder, {withFileTypes: true})
+    .then((value) => {
+      value.forEach(file => {
+        if (file.isFile()) {
+          const {name, ext} = path.parse(file.name);
+          stat(path.join(folder, file.name)).then( value => {
+            const size = (value.size / 1024).toFixed(2) + 'kb';
+            console.log(`${name} - ${ext.slice(1)} - ${size}`);
+          });
+        }
+      });
+    }).catch(error => console.error(error.message));
+} catch (error) {
+  console.error(error.message);
+}
+
+
